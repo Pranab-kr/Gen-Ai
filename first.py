91a9ed7a20e1f3d630b76ecc2195135ce53fa79f
pranab <pranabm406@gmail.com>
2025-11-06 23:59:02 +0530

    venv setup

--- first.py
+++
@@ -1,8 +0,0 @@
-def greet(name):
-    return f"Hello, {name}!"
-
-if(__name__ == "__main__"):
-    greeting = greet("Alice")
-    print(greeting)
-    print("Hello, World!")
-    print("This is the first.py file.")
