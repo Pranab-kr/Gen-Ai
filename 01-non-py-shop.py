ffb6e9817e2c178fc4d64caabf149083cb64ed9a
pranab <pranabm406@gmail.com>
2025-11-06 23:47:01 +0530

    first commit

--- 01-non-py-shop.py
+++ 01-non-py-shop.py
@@ -1,14 +1,14 @@
 class chai:
-  def __init__(self, sweetness,milk_level):
-    self.sweetness = sweetness
-    self.milk_level = milk_level
+    def __init__(self, sweetness,milk_level):
+      self.sweetness = sweetness
+      self.milk_level = milk_level
 
-  def sip(self):
-    print("Sipping the chai with sweetness level", self.sweetness, "and milk level", self.milk_level)
+    def sip(self):
+      print("Sipping the chai with sweetness level", self.sweetness, "and milk level", self.milk_level)
 
-  def add_sugar(self, amount):
-      self.sweetness += amount
-      print("Added", amount, "sugar. New sweetness level is", self.sweetness)
+    def add_sugar(self, amount):
+        self.sweetness += amount
+        print("Added", amount, "sugar. New sweetness level is", self.sweetness)
 
 
 my_chai = chai(sweetness=5, milk_level=8)
