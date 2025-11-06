a412e8c09ce9bdfb6900a61a2dea864995a776bf
pranab <pranabm406@gmail.com>
2025-11-06 23:39:27 +0530

    first-q

---
+++ 01-non-py-shop.py
@@ -0,0 +1,18 @@
+class chai:
+  def __init__(self, sweetness,milk_level):
+    self.sweetness = sweetness
+    self.milk_level = milk_level
+
+  def sip(self):
+    print("Sipping the chai with sweetness level", self.sweetness, "and milk level", self.milk_level)
+
+  def add_sugar(self, amount):
+      self.sweetness += amount
+      print("Added", amount, "sugar. New sweetness level is", self.sweetness)
+
+
+my_chai = chai(sweetness=5, milk_level=8)
+
+my_chai.sip()
+my_chai.add_sugar(2)
+my_chai.sip()
---
+++ first.py
@@ -0,0 +1,8 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+if(__name__ == "__main__"):
+    greeting = greet("Alice")
+    print(greeting)
+    print("Hello, World!")
+    print("This is the first.py file.")
