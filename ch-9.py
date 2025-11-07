0726a02a14005b8918ebd32497f79ceb677eb3c3
pranab <pranabm406@gmail.com>
2025-11-08 04:30:23 +0530

    sec-2 ch-9

---
+++ ch-9.py
@@ -0,0 +1,16 @@
+# set
+essential_spices = {"cinnamon", "nutmeg", "ginger"}
+optional_spices = {"clove", "allspice", "nutmeg"}
+
+all_spices = essential_spices | optional_spices
+
+print(all_spices)
+
+common_spices = essential_spices & optional_spices
+
+print(common_spices)
+
+only_in_essential = essential_spices - optional_spices
+print(only_in_essential)
+
+print(f"is clove an optional? {'clove' in optional_spices}")
