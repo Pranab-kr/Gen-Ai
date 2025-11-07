6200f93bc80aa7c1103ae00c68cbd25a22c7e921
pranab <pranabm406@gmail.com>
2025-11-08 00:15:49 +0530

    sec-2 ch-8

---
+++ ch-8.py
@@ -0,0 +1,35 @@
+# Working with Lists in Python like arrays
+
+ingredients = ["Cumin", "Coriander", "Cardamom", "Ginger"]
+ingredients.append("Cloves")
+
+print(f"Updated ingredients list: {ingredients}")
+ingredients.remove("Ginger")
+print(f"Ingredients list after removing Ginger: {ingredients}")
+
+
+spice_options = ["Black Pepper", "Cinnamon", "Nutmeg"]
+chai_ingredients = ["water", "milk"]
+
+chai_ingredients.extend(spice_options)
+print(f"Chai ingredients: {chai_ingredients}")
+
+chai_ingredients.insert(2, "tea leaves") # Inserting tea leaves adding at index 2
+print(f"Chai ingredients after adding tea leaves: {chai_ingredients}")
+
+lastadded_spice = chai_ingredients.pop() # Removing last added spice
+print(f"Removed last added spice: {lastadded_spice}")
+print(f"Chai ingredients after removing last added spice: {chai_ingredients}")
+
+# reversing the list
+chai_ingredients.reverse()
+print(f"Chai ingredients in reverse order: {chai_ingredients}")
+
+# sorting the list
+chai_ingredients.sort()
+print(f"Chai ingredients sorted alphabetically: {chai_ingredients}")
+
+# max & min
+sugar_levels = [5, 10, 3, 8, 2]
+print(f"Maximum sugar level: {max(sugar_levels)}")
+print(f"Minimum sugar level: {min(sugar_levels)}")
