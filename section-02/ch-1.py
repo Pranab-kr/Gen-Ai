ff0b611b96de02c5fbb5929c31cc4d41ee61808c
pranab <pranabm406@gmail.com>
2025-11-07 23:16:32 +0530

    sec-2 ch-1

---
+++ section-02/ch-1.py
@@ -0,0 +1,12 @@
+sugar_amount = 10
+
+print(f"First sugar amount: {sugar_amount}")
+
+sugar_amount = 20
+
+print(f"Second sugar amount: {sugar_amount}")
+
+print(f"Id of first sugar amount: {id(10)}")
+print(f"Id of second sugar amount: {id(20)}")
+
+# the value/number is not mutable, the reference is changed to point to a new value in memory
---
+++ section-02/ch-2.py
@@ -0,0 +1,10 @@
+spice_mix = set()
+print(f"Initial spice mix: {id(spice_mix)}")
+
+spice_mix.add("cinnamon")
+spice_mix.add("nutmeg")
+print(f"Spice mix after additions: {id(spice_mix)}")
+
+print(spice_mix)
+
+# the set object is mutable, so the reference remains the same even after modifications
