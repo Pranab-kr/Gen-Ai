77aa1ee395c18de84ab3be228c11518dbdba0bfb
pranab <pranabm406@gmail.com>
2025-11-10 23:24:28 +0530

    sec-4 ch-2

---
+++ section-04/ch-2.py
@@ -0,0 +1,11 @@
+# A chai shop makes tea in batches every 15 minutes.
+# You want to simulate 4 batches.
+# Task:
+
+# Use range() to simulate batch numbers.
+
+# « Print: "Preparing chai for batch #[number]*
+
+for batch_number in range(1, 5):
+    print(f"Preparing chai for batch #{batch_number}")
+    print("Chai is ready! Serving now.")
