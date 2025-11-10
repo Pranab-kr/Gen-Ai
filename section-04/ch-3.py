27eeb9fa37bbc733dda7d6b29c4458c1b07af5e2
pranab <pranabm406@gmail.com>
2025-11-10 23:27:27 +0530

    sec-4 ch-3

---
+++ section-04/ch-3.py
@@ -0,0 +1,12 @@
+# You receive a list of names for chai orders.
+# The goal is to print out the order queue.
+# Task:
+
+# « Use a list of names.
+
+# « Print: "Order ready for [name]"
+
+names = ["ram", "shyam", "geeta", "sita", "laxman"]
+
+for name in names:
+    print(f"Order ready for {name}")
