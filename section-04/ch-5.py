305bebde5b00cb29dd6923716d6379da898b1e4f
pranab <pranabm406@gmail.com>
2025-11-10 23:37:13 +0530

    sec-4 ch-5

---
+++ section-04/ch-5.py
@@ -0,0 +1,13 @@
+# You're preparing an order summary with customer names
+# and their total bill.
+# Task:
+
+# « Use two lists: one for names and one for bills.
+
+# « Print: "[Name] paid I[amount]"
+
+names =  ["Alice", "ram", "pkm", "sita"]
+bills = [50, 30, 70, 40]
+
+for name, bill in zip(names, bills):
+    print(f"{name} paid ₹{bill}")
