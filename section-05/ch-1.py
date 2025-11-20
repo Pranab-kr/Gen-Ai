9c8ff2f1b99c5eb953585f24472f62d1c12014bd
pranab <pranabm406@gmail.com>
2025-11-20 23:26:40 +0530

    ch-1

---
+++ section-05/ch-1.py
@@ -0,0 +1,15 @@
+# You're managing a busy tea stall.
+# You receive many orders and want to print each customer's name
+# along with the type of chai they ordered.
+# Task:
+# * Write a function print_order(name, chai_type)
+# + Call it multiple times for different customers
+
+
+def print_order(name, chai_type):
+    print(f"{name}, order chai {chai_type}!")
+
+
+print_order("Ram", "Masala")
+print_order("Pranab", "Ginger")
+print_order("Krish", "Cinnamond")
