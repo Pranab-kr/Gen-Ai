46a4867f35f741392bf31923c97a3f083b954455
pranab <pranabm406@gmail.com>
2025-11-20 23:40:26 +0530

    ch-4

---
+++ section-05/ch-4.py
@@ -0,0 +1,18 @@
+# You sell different chai sizes.
+# Instead of writing formulas everywhere, create a function.
+# Task:
+
+# * Write calculate_bill(cups, price_per_cup)
+
+# « Return total bill
+
+# « Use this function for multiple orders
+
+
+def calculate_bill(cups, price_per_cup):
+    total_bill = cups * price_per_cup
+    return total_bill
+
+
+calculate_bill_1 = calculate_bill(3, 5)
+print(f"Total bill for order 1 is: {calculate_bill_1}")
